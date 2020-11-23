f10cdae1c25b83c862dc3189e3c54eec5d772773
joseffffff <joseffff@protonmail.com>
2020-11-23 22:47:00 +0100

    form working

--- js/singleproperty.js
+++ js/singleproperty.js
@@ -65,9 +65,51 @@ window.addEventListener('load', () => {
         }
       });
 
+      const contactForm = document.getElementById('contact-form');
+      contactForm.onsubmit = onSubmitContactForm;
     });
 });
 
 function formatDigit(value) {
   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+function onSubmitContactForm(event) {
+  event.preventDefault();
+
+  const projectId = window.location.search.split('=')[1]
+
+  const nameInput = document.getElementById('inputName');
+  const emailInput = document.getElementById('inputEmail');
+  const phoneInput = document.getElementById('inputPhone');
+  const messageInput = document.getElementById('textMessage');
+
+  const formData = new FormData();
+  formData.append('name', nameInput.value);
+  formData.append('email', emailInput.value);
+  formData.append('phone', phoneInput.value);
+  formData.append('comment', messageInput.value);
+
+  window.fetch(`https://admin.pisosautoproject.com/api/v1/projects/${projectId}/contact`, {
+    method: 'POST',
+    body: formData,
+  }).then(response => {
+    response.json().then(jsonResponse => {
+      if (jsonResponse.success) {
+        Swal.fire({
+          title: 'Fet!',
+          text: 'Formulari enviat.',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then(() => {
+          nameInput.value = '';
+          emailInput.value = '';
+          phoneInput.value = '';
+          messageInput.value = '';
+        });
+      }
+    });
+  });
+
+  return false;
 }
