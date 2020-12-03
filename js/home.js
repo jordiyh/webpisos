97a3b020594995c3b65aabc256948acac18ee453
joseffffff <joseffff@protonmail.com>
2020-12-03 23:25:23 +0100

    codestyle

--- js/home.js
+++ js/home.js
@@ -45,7 +45,10 @@ window.addEventListener('load', () => {
         margin: -1,
         items: 1,
         nav: true,
-        navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
+        navText: [
+          '<i class="ion-ios-arrow-back" aria-hidden="true"></i>',
+          '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'
+        ],
         autoplay: true,
         autoplayTimeout: 3000,
         autoplayHoverPause: true
